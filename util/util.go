5213d8dac0b41547675e158321cd53b13f39ef7f
Andrew Pillar <me@andrewpillar.com>
2018-08-25 19:52:57 +0100

    Add Copy function

--- util/util.go
+++ util/util.go
@@ -4,12 +4,80 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"unicode"
 )
+
+func Copy(src, dst string) error {
+	info, err := os.Stat(src)
+
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return CopyDir(src, dst, info)
+	}
+
+	return CopyFile(src, dst, info)
+}
+
+func CopyDir(src, dst string, info os.FileInfo) error {
+	if err := os.MkdirAll(dst, info.Mode()); err != nil {
+		return err
+	}
+
+	files, err := ioutil.ReadDir(src)
+
+	if err != nil {
+		return err
+	}
+
+	for _, f := range files {
+		fdst := filepath.Join(dst, f.Name())
+		fsrc := filepath.Join(src, f.Name())
+
+		if err := Copy(fsrc, fdst); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func CopyFile(src, dst string, info os.FileInfo) error {
+	if err := os.MkdirAll(filepath.Dir(dst), info.Mode()); err != nil {
+		return err
+	}
+
+	fdst, err := os.Create(dst)
+
+	if err != nil {
+		return err
+	}
+
+	defer fdst.Close()
+
+	if err = os.Chmod(fdst.Name(), info.Mode()); err != nil {
+		return err
+	}
+
+	fsrc, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	defer fsrc.Close()
+
+	_, err = io.Copy(fdst, fsrc)
+
+	return err
+}
 
 func Deslug(s string) string {
 	parts := strings.Split(s, "-")
