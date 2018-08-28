9aa5d6c3a510219aeed0b902c7b667e6f4adf62c
Andrew Pillar <me@andrewpillar.com>
2018-08-28 22:03:27 +0100

    Refactor publish logic

--- command/publish.go
+++ command/publish.go
@@ -10,53 +10,54 @@ import (
 	"strings"
 	"sync"
 	"text/template"
+	"time"
 
 	"github.com/andrewpillar/cli"
 
 	"github.com/andrewpillar/jrnl/meta"
 	"github.com/andrewpillar/jrnl/post"
-//	"github.com/andrewpillar/jrnl/usage"
+	"github.com/andrewpillar/jrnl/usage"
 	"github.com/andrewpillar/jrnl/util"
 )
 
 var (
-	// Map used to store all of the different paths that can have posts indexed
-	// under them. All the keys in the map will be different degraded paths
-	// from the post's source, for example:
-	//
-	//   _site/some-category/2006/01/02
-	//   _site/2006/01/02
-	//   _site/2006/01
-	//   _site/2006
-	//   _site
-	postIndexes map[string]*post.Store
+	journalTitle string
 
-	categoryDatePattern = "[-_a-zA-Z0-9]+/[-a-zA-Z0-9/]+/[0-9]{4}/[0-9]{2}/[0-9]{2}"
+	postIndexes = make(map[string]*post.Store)
 
-	categoryPattern = "[-_a-zA-Z0-9]+/[-a-zA-Z0-9/]"
+	yearPattern = "[_site]/[0-9]{4}"
 
-	dayPattern = "[-_a-zA-Z0-9]+/[0-9]{4}/[0-9]{2}/[0-9]{2}"
+	monthPattern = "[_site]/[0-9]{4}/[0-9]{2}"
 
-	monthPattern = "[-_a-zA-Z0-9]+/[0-9]{4}/[0-9]{2}"
+	dayPattern = "[_site]/[0-9]{4}/[0-9]{2}/[0-9]{2}"
 
-	yearPattern = "[-_a-zA-Z0-9]+/[0-9]{4}"
+	categoryYearPattern = "[_site]/[-a-z0-9/]+/[0-9]{4}"
 
-	categoryDateRegex = regexp.MustCompile(categoryDatePattern)
+	categoryMonthPattern = "[_site]/[-a-z0-9/]+/[0-9]{4}/[0-9]{2}"
 
-	categoryRegex = regexp.MustCompile(categoryPattern)
+	categoryDayPattern = "[_site]/[-a-z0-9/]+/[0-9]{4}/[0-9]{2}/[0-9]{2}"
 
-	dayRegex = regexp.MustCompile(dayPattern)
+	categoryPattern = "[_site]/[-a-z0-9/]+"
+
+	yearRegex = regexp.MustCompile(yearPattern)
 
 	monthRegex = regexp.MustCompile(monthPattern)
 
-	yearRegex = regexp.MustCompile(yearPattern)
+	dayRegex = regexp.MustCompile(dayPattern)
+
+	categoryYearRegex = regexp.MustCompile(categoryYearPattern)
+
+	categoryMonthRegex = regexp.MustCompile(categoryMonthPattern)
+
+	categoryDayRegex = regexp.MustCompile(categoryDayPattern)
+
+	categoryRegex = regexp.MustCompile(categoryPattern)
 )
 
 func publishPost(
 	p *post.Post,
-	title string,
 	wg *sync.WaitGroup,
-	posts chan *post.Post,
+	published chan *post.Post,
 	errs chan error,
 ) {
 	defer wg.Done()
@@ -75,7 +76,7 @@ func publishPost(
 
 	defer f.Close()
 
-	layout, err := ioutil.ReadAll(f)
+	b, err := ioutil.ReadAll(f)
 
 	if err != nil {
 		errs <- err
@@ -84,16 +85,33 @@ func publishPost(
 
 	p.Convert()
 
-	if err := p.Publish(title, string(layout)); err != nil {
+	if err = p.Publish(journalTitle, string(b)); err != nil {
 		errs <- err
 		return
 	}
 
-	posts <- p
+	published <- p
 }
 
-// Add the given post to all of the possible path indexes that are available.
-func addPostToIndex(p *post.Post, wg *sync.WaitGroup, m *sync.Mutex) {
+func publishPosts(posts post.Store, errs chan error) chan *post.Post {
+	wg := &sync.WaitGroup{}
+	published := make(chan *post.Post)
+
+	for _, p := range posts {
+		wg.Add(1)
+
+		go publishPost(p, wg, published, errs)
+	}
+
+	go func() {
+		wg.Wait()
+		close(published)
+	}()
+
+	return published
+}
+
+func indexPost(p *post.Post, wg *sync.WaitGroup, m *sync.Mutex) {
 	defer wg.Done()
 
 	parts := strings.Split(filepath.Dir(p.SitePath), string(os.PathSeparator))
@@ -121,128 +139,253 @@ func addPostToIndex(p *post.Post, wg *sync.WaitGroup, m *sync.Mutex) {
 	}
 }
 
+func indexPosts(posts chan *post.Post) *sync.WaitGroup {
+	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
+
+	for p := range posts {
+		wg.Add(1)
+
+		go indexPost(p, wg, m)
+	}
+
+	return wg
+}
+
+func writeIndexFile(layout, index string, data interface{}) error {
+	if layout == "" {
+		return errors.New("no layout for index " + index)
+	}
+
+	if data == nil {
+		return errors.New("no data for index " + index)
+	}
+
+	flayout, err := os.Open(layout)
+
+	if err != nil {
+		return err
+	}
+
+	defer flayout.Close()
+
+	findex, err := os.OpenFile(index, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0660)
+
+	if err != nil {
+		return err
+	}
+
+	defer findex.Close()
+
+	b, err := ioutil.ReadAll(flayout)
+
+	if err != nil {
+		return err
+	}
+
+	t, err := template.New("index").Parse(string(b))
+
+	if err != nil {
+		return err
+	}
+
+	if err = t.Execute(findex, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func writeIndex(
-	dir, title string,
-	posts *post.Store,
+	dir string,
+	posts post.Store,
 	wg *sync.WaitGroup,
 	errs chan error,
 ) {
 	defer wg.Done()
 
-	indexPath := filepath.Join(dir, "index.html")
+	index := filepath.Join(dir, "index.html")
+	layout := ""
 
-	layoutPath := ""
-
-	var page interface{}
-
-	pattern := []byte(dir)
+	var data interface{}
 
 	if dir == meta.SiteDir {
-		layoutPath = filepath.Join(meta.LayoutsDir, meta.IndexLayout)
+		layout = filepath.Join(meta.LayoutsDir, meta.IndexLayout)
 
-		page = struct{
+		data = struct{
 			Title string
 			Posts post.Store
 		}{
-			Title: title,
-			Posts: *posts,
+			Title: journalTitle,
+			Posts: posts,
 		}
+
+		if err := writeIndexFile(layout, index, data); err != nil {
+			errs <- err
+			return
+		}
+
+		return
 	}
 
-	if categoryDateRegex.Match(pattern) || categoryRegex.Match(pattern) {
-		layoutPath = filepath.Join(meta.LayoutsDir, meta.CategoryIndexLayout)
+	pattern := []byte(dir)
 
-		page = struct{
+	parts := strings.Split(dir, string(os.PathSeparator))
+
+	notCategory := false
+
+	timeFormat := ""
+	timeIndex := 0
+
+	if dayRegex.Match(pattern) {
+		notCategory = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.DayIndexLayout)
+
+		timeFormat = filepath.Join("2006", "01", "02")
+		timeIndex = 3
+	} else if monthRegex.Match(pattern) {
+		notCategory = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.MonthIndexLayout)
+
+		timeFormat = filepath.Join("2006", "01")
+		timeIndex = 2
+	} else if yearRegex.Match(pattern) {
+		notCategory = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.YearIndexLayout)
+
+		timeFormat = filepath.Join("2006")
+		timeIndex = 1
+	}
+
+	if notCategory {
+		t, err := time.Parse(
+			timeFormat,
+			filepath.Join(parts[len(parts) - timeIndex:]...),
+		)
+
+		if err != nil {
+			errs <- err
+			return
+		}
+
+		data = struct{
+			Title string
+			Time  time.Time
+			Posts post.Store
+		}{
+			Title: journalTitle,
+			Time:  t,
+			Posts: posts,
+		}
+
+		if err = writeIndexFile(layout, index, data); err != nil {
+			errs <- err
+			return
+		}
+
+		return
+	}
+
+	categoryDate := false
+
+	if categoryDayRegex.Match(pattern) {
+		categoryDate = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.CategoryDayIndexLayout)
+
+		timeFormat = filepath.Join("2006", "01", "02")
+		timeIndex = 3
+	} else if categoryMonthRegex.Match(pattern) {
+		categoryDate = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.CategoryMonthIndexLayout)
+
+		timeFormat = filepath.Join("2006", "01")
+		timeIndex = 2
+	} else if categoryYearRegex.Match(pattern) {
+		categoryDate = true
+
+		layout = filepath.Join(meta.LayoutsDir, meta.CategoryYearIndexLayout)
+
+		timeFormat = filepath.Join("2006")
+		timeIndex = 1
+	}
+
+	if categoryDate {
+		category := util.Deslug(strings.Join(parts[1:len(parts) - timeIndex], " / "))
+
+		t, err := time.Parse(
+			timeFormat,
+			filepath.Join(parts[len(parts) - timeIndex:]...),
+		)
+
+		if err != nil {
+			errs <- err
+			return
+		}
+
+		data = struct{
+			Title    string
+			Category string
+			Time     time.Time
+			Posts    post.Store
+		}{
+			Title:    journalTitle,
+			Category: category,
+			Time:     t,
+			Posts:    posts,
+		}
+
+		if err = writeIndexFile(layout, index, data); err != nil {
+			errs <- err
+			return
+		}
+
+		return
+	}
+
+	if categoryRegex.Match(pattern) {
+		category := strings.Join(parts[1:], " / ")
+
+		layout = filepath.Join(meta.LayoutsDir, meta.CategoryIndexLayout)
+
+		data = struct{
 			Title    string
 			Category string
 			Posts    post.Store
 		}{
-			Title:    title,
-			Category: "category here",
-			Posts:    *posts,
+			Title:    journalTitle,
+			Category: category,
+			Posts:    posts,
 		}
-	} else if dayRegex.Match(pattern) {
-		layoutPath = filepath.Join(meta.LayoutsDir, meta.DayIndexLayout)
 
-		page = struct{
-			Title string
-			Posts post.Store
-		}{
-			Title: title,
-			Posts: *posts,
+		if err := writeIndexFile(layout, index, data); err != nil {
+			errs <- err
+			return
 		}
-	} else if monthRegex.Match(pattern) {
-		layoutPath = filepath.Join(meta.LayoutsDir, meta.MonthIndexLayout)
 
-		page = struct{
-			Title string
-			Posts post.Store
-		}{
-			Title: title,
-			Posts: *posts,
-		}
-	} else if yearRegex.Match(pattern) {
-		layoutPath = filepath.Join(meta.LayoutsDir, meta.YearIndexLayout)
-
-		page = struct{
-			Title string
-			Posts post.Store
-		}{
-			Title: title,
-			Posts: *posts,
-		}
-	}
-
-	if layoutPath == "" {
-		errs <- errors.New("no layout for index to " + dir)
 		return
 	}
 
-	if page == nil {
-		errs <- errors.New("no page to write index to " + dir)
-	}
+	errs <- errors.New("could not match pattern to dir " + dir)
+}
 
-	layoutFile, err := os.Open(layoutPath)
+func writeIndexes(wg *sync.WaitGroup, errs chan error) {
+	for dir, posts := range postIndexes {
+		wg.Add(1)
 
-	if err != nil {
-		errs <- err
-		return
-	}
-
-	defer layoutFile.Close()
-
-	layout, err := ioutil.ReadAll(layoutFile)
-
-	if err != nil {
-		errs <- err
-		return
-	}
-
-	t, err := template.New("index").Parse(string(layout))
-
-	if err != nil {
-		errs <- err
-		return
-	}
-
-	f, err := os.OpenFile(indexPath, os.O_TRUNC|os.O_RDWR|os.O_CREATE, 0660)
-
-	if err != nil {
-		errs <- err
-		return
-	}
-
-	if err = t.Execute(f, page); err != nil {
-		errs <- err
-		return
+		go writeIndex(dir, *posts, wg, errs)
 	}
 }
 
 func Publish(c cli.Command) {
-//	if c.Flags.IsSet("help") {
-//		fmt.Println(usage.Publish)
-//		return
-//	}
+	if c.Flags.IsSet("help") {
+		fmt.Println(usage.Publish)
+		return
+	}
 
 	mustBeInitialized()
 
@@ -260,48 +403,23 @@ func Publish(c cli.Command) {
 		util.Error("failed to read meta file", err)
 	}
 
+	journalTitle = m.Title
+
 	r := post.NewResolver()
 
 	posts := r.Resolve()
 
-	postIndexes = make(map[string]*post.Store)
-
-	wg := &sync.WaitGroup{}
-
-	published := make(chan *post.Post)
 	errs := make(chan error)
 
-	for _, p := range posts {
-		wg.Add(1)
+	published := publishPosts(posts, errs)
 
-		go publishPost(p, m.Title, wg, published, errs)
-	}
-
-	go func() {
-		wg.Wait()
-
-		close(published)
-	}()
-
-	mut := &sync.Mutex{}
-
-	for p := range published {
-		wg.Add(1)
-
-		go addPostToIndex(p, wg, mut)
-	}
-
+	wg := indexPosts(published)
 	wg.Wait()
 
-	for dir, posts := range postIndexes {
-		wg.Add(1)
-
-		go writeIndex(dir, m.Title, posts, wg, errs)
-	}
+	writeIndexes(wg, errs)
 
 	go func() {
 		wg.Wait()
-
 		close(errs)
 	}()
 
