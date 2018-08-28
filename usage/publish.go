a1c9e9d7078147a45ec038d451bf163eb0a957b1
Andrew Pillar <andrewjohnpillar@gmail.com>
2018-08-28 22:02:59 +0100

    Add publish command to usage

---
+++ usage/publish.go
@@ -0,0 +1,15 @@
+package usage
+
+var Publish = `jrnl publish - Publish the journal's posts
+
+Usage:
+
+  jrnl publish [options...]
+
+Options:
+
+  -d, --draft           Publish a draft, this won't copy the site to the remote
+  -r, --remote=<alias>  The remote to copy the published site to. If none is
+                        given then the default will be used if set.
+
+  --help  Display this usage message.`
