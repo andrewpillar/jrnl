b0698546a1b7a915f5bcd2151a69f7d8c453d955
Andrew Pillar <me@andrewpillar.com>
2018-10-06 10:32:55 +0100

    Add verbose option for 'remote ls', and formatting

--- command/remote.go
+++ command/remote.go
@@ -3,66 +3,99 @@ package command
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/andrewpillar/cli"
 
 	"github.com/andrewpillar/jrnl/meta"
-	"github.com/andrewpillar/jrnl/usage"
 	"github.com/andrewpillar/jrnl/util"
 )
 
-func Remote(c cli.Command) {
-	fmt.Println(usage.Remote)
-}
+var (
+	paddedFmt = "%%%ds - %%%ds"
+
+	errRemoteFindFmt = "%s: could not find remote: %s\n"
+)
 
 func RemoteLs(c cli.Command) {
-	if c.Flags.IsSet("help") {
-		fmt.Println(usage.RemoteLs)
-		return
-	}
-
-	mustBeInitialized()
+	util.MustBeInitialized()
 
 	m, err := meta.Open()
 
 	if err != nil {
-		util.Error("failed to open meta file", err)
+		util.Exit("failed to open meta file", err)
+	}
+
+	m.Close()
+
+	verbose := c.Flags.IsSet("verbose")
+
+	aliasLen := 0
+	targetLen := 0
+
+	for k, v := range m.Remotes {
+		if len(k) > aliasLen {
+			aliasLen = len(k)
+		}
+
+		if len(v.Target) > targetLen {
+			targetLen = len(v.Target)
+		}
 	}
 
 	for k, v := range m.Remotes {
-		fmt.Printf("%s - %s", k, v.Target)
+		if verbose {
+			fmt.Println("---")
+			fmt.Printf("Alias:    %s", k)
 
-		if m.Default == k {
-			fmt.Printf("    [default]")
+			if m.Default == k {
+				fmt.Printf("  [default]")
+			}
+
+			fmt.Printf("\n")
+
+			fmt.Println("Target:  ", v.Target)
+
+			if !filepath.IsAbs(v.Target) {
+				fmt.Println("Port:    ", v.Port)
+				fmt.Println("Identity:", v.Identity)
+			}
+		} else {
+			fmt.Printf(fmt.Sprintf(paddedFmt, -aliasLen, -targetLen), k, v.Target)
+
+			if m.Default == k {
+				fmt.Printf("  [default]")
+			}
+
+			fmt.Printf("\n")
 		}
-
-		fmt.Printf("\n")
 	}
 }
 
 func RemoteSet(c cli.Command) {
-	if c.Flags.IsSet("help") || len(c.Args) == 0 {
-		fmt.Println(usage.RemoteSet)
-		return
-	}
+	util.MustBeInitialized()
 
 	alias := c.Args.Get(0)
 	target := c.Args.Get(1)
 
+	if alias == "" {
+		util.Exit("missing alias", nil)
+	}
+
 	if target == "" {
-		util.Error("missing remote target", nil)
+		util.Exit("missing target", nil)
 	}
 
 	port, err := c.Flags.GetInt("port")
 
 	if err != nil {
-		util.Error("failed to get port number from flag", err)
+		util.Exit("failed to get port number", err)
 	}
 
 	m, err := meta.Open()
 
 	if err != nil {
-		util.Error("failed to open meta file", err)
+		util.Exit("failed to open meta file", err)
 	}
 
 	defer m.Close()
@@ -72,8 +105,10 @@ func RemoteSet(c cli.Command) {
 		Port:   port,
 	}
 
-	if c.Flags.GetString("identity") != "" {
-		r.Identity = c.Flags.GetString("identity")
+	identity := c.Flags.GetString("identity")
+
+	if identity != "" {
+		r.Identity = identity
 	}
 
 	m.Remotes[alias] = r
@@ -83,23 +118,18 @@ func RemoteSet(c cli.Command) {
 	}
 
 	if err := m.Save(); err != nil {
-		util.Error("faild to save meta file", err)
+		util.Exit("failed to save meta file", err)
 	}
 }
 
 func RemoteRm(c cli.Command) {
-	if c.Flags.IsSet("help") || len(c.Args) == 0 {
-		fmt.Println(usage.RemoteRm)
-		return
-	}
+	util.MustBeInitialized()
 
 	m, err := meta.Open()
 
 	if err != nil {
-		util.Error("failed to open meta file", err)
+		util.Exit("failed to open meta file", err)
 	}
-
-	defer m.Close()
 
 	code := 0
 
@@ -107,10 +137,9 @@ func RemoteRm(c cli.Command) {
 		_, ok := m.Remotes[alias]
 
 		if !ok {
-			fmt.Fprintf(os.Stderr, "jrnl: could not find remote: %s\n", alias)
+			fmt.Fprintf(os.Stderr, errRemoteFindFmt, os.Args[0], alias)
 
 			code = 1
-
 			continue
 		}
 
@@ -122,7 +151,7 @@ func RemoteRm(c cli.Command) {
 	}
 
 	if err := m.Save(); err != nil {
-		util.Error("faild to save meta file", err)
+		util.Exit("failed to save meta file", err)
 	}
 
 	os.Exit(code)
