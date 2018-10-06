845cb4ebc4238d783345a427faebcb765dcc0e42
Andrew Pillar <andrewjohnpillar@gmail.com>
2018-10-06 10:32:33 +0100

    Remove usage package, use Exit

--- command/title.go
+++ command/title.go
@@ -6,20 +6,14 @@ import (
 	"github.com/andrewpillar/cli"
 
 	"github.com/andrewpillar/jrnl/meta"
-	"github.com/andrewpillar/jrnl/usage"
 	"github.com/andrewpillar/jrnl/util"
 )
 
 func Title(c cli.Command) {
-	if c.Flags.IsSet("help") {
-		fmt.Println(usage.Title)
-		return
-	}
-
 	m, err := meta.Open()
 
 	if err != nil {
-		util.Error("failed to open meta file", err)
+		util.Exit("failed to open meta file", err)
 	}
 
 	title := c.Args.Get(0)
@@ -37,7 +31,7 @@ func Title(c cli.Command) {
 	m.Title = title
 
 	if err := m.Save(); err != nil {
-		util.Error("failed to save meta file", err)
+		util.Exit("failed to save meta file", err)
 	}
 
 	m.Close()
