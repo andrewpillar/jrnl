e570c863039567efe1a9999f4b2530314d2f0b54
Andrew Pillar <me@andrewpillar.com>
2018-10-27 20:56:18 +0100

    Update commands to work with pages, and posts

--- command/ls.go
+++ command/ls.go
@@ -6,46 +6,40 @@ import (
 
 	"github.com/andrewpillar/cli"
 
+	"github.com/andrewpillar/jrnl/page"
 	"github.com/andrewpillar/jrnl/post"
 	"github.com/andrewpillar/jrnl/util"
 )
 
-func printPost(p post.Post, verbose bool) {
-	if verbose {
-		fmt.Println("---")
-		fmt.Println("ID:       ", p.ID)
-		fmt.Println("Title:    ", p.Title)
-		fmt.Println("Category: ", p.Category.Name)
-		fmt.Println("Source:   ", p.SourcePath)
-		fmt.Println("Site:     ", p.SitePath)
-		fmt.Println("Created:  ", p.CreatedAt.Format(post.DateLayout))
-		fmt.Println("Updated:  ", p.UpdatedAt.Format(post.DateLayout))
-		return
-	}
-
-	fmt.Println(p.ID)
-}
-
 func Ls(c cli.Command) {
 	util.MustBeInitialized()
 
-	posts, err := post.ResolvePosts()
+	pages, err := page.All()
 
 	if err != nil {
-		util.Exit("failed to resolve posts", err)
+		util.Exit("failed to get all pages", err)
+	}
+
+	posts, err := post.All()
+
+	if err != nil {
+		util.Exit("failed to get all posts", err)
+	}
+
+	for _, p := range pages {
+		fmt.Println(p.ID)
 	}
 
 	category := c.Flags.GetString("category")
-	verbose := c.Flags.IsSet("verbose")
 
 	for _, p := range posts {
 		if category == "" {
-			printPost(p, verbose)
+			fmt.Println(p.ID)
 			continue
 		}
 
 		if strings.ToLower(category) == strings.ToLower(p.Category.Name) {
-			printPost(p, verbose)
+			fmt.Println(p.ID)
 		}
 	}
 }
