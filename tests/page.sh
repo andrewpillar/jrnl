53f33169ceab08aa9a436a6680123038e64bb65d
Andrew Pillar <andrewjohnpillar@gmail.com>
2019-01-23 17:40:37 +0000

    Remove seconds from example timestamps

--- tests/page.sh
+++ tests/page.sh
@@ -7,23 +7,27 @@ title: About
 layout: \"\"
 ---\n"
 
+title="About"
+id="about"
+
 pushd $(mktemp -d) > /dev/null
 
 jrnl init > /dev/null
-EDITOR=./tests/editor.sh jrnl page About > /dev/null
+EDITOR=./tests/editor.sh jrnl page "$title" > /dev/null
 
-if [ -f _pages/about.md ]; then
-	printf "[  OK  ] page 'About' created\n"
-else
-	printf "[FAILED] page 'About' not created\n"
+if ! jrnl ls | grep -q "$id"; then
+	printf "[FAILED] could not find '$id' with 'jrnl ls'\n"
+	jrnl ls
 	exit 1
 fi
+
+printf "[  OK  ] found '$id' with 'jrnl ls'\n"
 
 expected=$(mktemp)
 actual=$(mktemp)
 
 printf "%b" "$front_matter" > $expected
-head -n 4 _pages/about.md > $actual
+head -n 4 _pages/"$id".md > $actual
 
 if ! diff $expected $actual > /dev/null; then
 	printf "[FAILED] front matter does not match expected\n"
