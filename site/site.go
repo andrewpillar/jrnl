013f5f85ca11409b121e73a0d3d10024293d42fb
Andrew Pillar <me@andrewpillar.com>
2019-06-19 21:53:21 +0100

    Use Link instead of Site

--- site/site.go
+++ site/site.go
@@ -5,10 +5,9 @@ import (
 	"github.com/andrewpillar/jrnl/page"
 )
 
-// Simple struct that will be passed to all layout templates.
 type Site struct {
 	Title      string
-	Site       string
+	Link       string
 	Categories []category.Category
 	Pages      []*page.Page
 }
